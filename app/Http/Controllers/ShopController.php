d688e842ebf96a22e22377e8c9a85fbc55adca37
Mahmoud Elzeer <mahmoudelzeer457@gmail.com>
2026-07-22 18:37:46 +0300

    Implement product details page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,9 +7,17 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $products = Product::orderby('created_at', 'DESC')->paginate(12);
         return view('shop', compact('products'));
     }
-    
+
+    public function product_details($product_slug)
+    {
+        $product = Product::where('slug', $product_slug)->firstOrFail();
+        $rproducts = Product::where('slug', '<>', $product_slug)->take(8)->get();
+        return view('details', compact('product', 'rproducts'));
+    }
+
 }
